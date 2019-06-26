06245537758f620a324595c1c9713c2b4e20e9a6
ixingjue <826739558@qq.com>
2019-06-26 13:57:41 +0800

    测试tp验证器

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -12,6 +12,12 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Hyperf\HttpServer\Annotation\RequestMapping;
+use think\Validate;
+
+/**
+ * @\Hyperf\HttpServer\Annotation\Controller()
+ */
 class IndexController extends Controller
 {
     public function index()
@@ -20,8 +26,33 @@ class IndexController extends Controller
         $method = $this->request->getMethod();
 
         return [
-            'method' => $method,
+            'method'  => $method,
             'message' => "Hello {$user}.",
         ];
+    }
+
+    /**
+     * 测试tp验证器
+     * @RequestMapping(path="test2", method="get,post")
+     */
+    public function test2()
+    {
+        try {
+            $validate = Validate::make([
+//                'name'  => 'require|max:25',
+                'email' => 'email'
+            ]);
+            $data = [
+//                'name'  => '11',
+                'email' => '9999'
+            ];
+            if (!$validate->check($data)) {
+                var_dump($validate->getError());
+                return $validate->getError();
+            }
+        } catch (\Throwable $e) {
+            var_dump(111);
+            return $e->getMessage();
+        }
     }
 }
